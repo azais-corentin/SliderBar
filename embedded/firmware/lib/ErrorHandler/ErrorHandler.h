5df6787942c3be0ccd7453a183ca657890492261
Azais Corentin <azaiscorentin@gmail.com>
2018-10-18 15:02:06 +0200

    Make ErrorHandler c compatible

--- embedded/firmware/lib/ErrorHandler/ErrorHandler.h
+++ embedded/firmware/lib/ErrorHandler/ErrorHandler.h
@@ -1,6 +1,11 @@
 #ifndef __ERRORHANDLER_H__
 #define __ERRORHANDLER_H__
 
+// Needed if a .c file includes this
+#ifdef __cplusplus
+extern "C" {
+#endif
+
 /**
   * @brief  This function is executed in case of error occurrence.
   * @param  file: The file name as string.
@@ -10,5 +15,10 @@
 void _Error_Handler(char* file, int line);
 
 #define Error_Handler() _Error_Handler(__FILE__, __LINE__)
+
+// Needed if a .c file includes this
+#ifdef __cplusplus
+}
+#endif
 
 #endif // __ERRORHANDLER_H__
